6fad8fa984dd664e7f6bd5ffad07323a5011e7a0
Hhhhnxsm <yinhan3273108824@outook.com>
2025-12-17 23:17:56 +0800

    Add module: server

--- commands.sh
+++ commands.sh
@@ -19,3 +19,15 @@ git commit -m "docs: add last&this para"
 git checkout main
 git merge docs --no-ff
 git push -u origin main
+
+# add server subrepo
+git submodule add https://github.com/NANNO35578/calcite_server.git calcite_server
+git commit -m "Add module: server"
+# 弄错了, 撤销一次
+git reset --soft HEAD~1
+git add .
+git commit -m "Add module: server"
+# 在calcite拉取最新的server:
+git submodule update --init --recursive
+
+
