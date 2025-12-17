16adc36f0a827a7f9882d51870d12cad184ca95d
Hhhhnxsm <yinhan3273108824@outook.com>
2025-12-17 17:11:58 +0800

    docs: init repository

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Intelligent note-taking system based on both Web and Android platforms
+<!-- 毕业设计-哈尔滨理工大学-2204020121 -->
+基于Web与Android双端的智能笔记系统
---
+++ commands.sh
@@ -0,0 +1,5 @@
+# 251217
+# init repository
+git init
+git remote add origin https://github.com/NANNO35578/calcite.git
+git pull origin main # pull license
